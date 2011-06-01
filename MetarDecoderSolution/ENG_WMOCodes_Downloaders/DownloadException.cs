90b84207ff74dbbdc2d2b5abcd390b1a7ae2132c
Engin1980_cp <engin@seznam.cz>
2011-06-01 19:32:37 +0000

    Added downloaders.

---
+++ MetarDecoderSolution/ENG_WMOCodes_Downloaders/DownloadException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENG.WMOCodes.Downloaders
+{
+  /// <summary>
+  /// Raised when some error occurs during metar downloading or decoding.
+  /// Inner exception should contain more accurate information.
+  /// </summary>
+#if SILVERLIGHT == false
+  [global::System.Serializable]
+#endif
+  public class DownloadException : Exception
+  {
+    public DownloadException(string message) : base(message) { }
+
+    public DownloadException(string message, Exception inner) : base(message, inner) { }
+
+#if SILVERLIGHT == FALSE
+
+    protected DownloadException(
+    System.Runtime.Serialization.SerializationInfo info,
+    System.Runtime.Serialization.StreamingContext context)
+      : base(info, context) { }
+
+#endif
+  }
+}
---
+++ MetarDecoderSolution/ENG_WMOCodes_Downloaders/Retrievers/TAF/NoaaGovRetriever.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENG.WMOCodes.Downloaders.Retrievers.TAF
+{
+  public class NoaaGovRetriever : IRetriever
+  {
+    #region IRetriever Members
+
+    private const string url = @"http://weather.noaa.gov/pub/data/forecasts/taf/stations/";
+    public string GetUrlForICAO(string icao)
+    {
+      return url + icao.ToUpper() + ".TXT";
+    }
+
+    public string DecodeWMOCode(System.IO.Stream sourceStream)
+    {
+      StringBuilder ret = new StringBuilder();
+
+      System.IO.StreamReader rdr = new System.IO.StreamReader(sourceStream);
+      rdr.ReadLine();
+      string line = rdr.ReadLine();
+      while (line != null)
+      {
+        ret.Append(line + " ");
+        line = rdr.ReadLine();
+      }
+
+      return ret.ToString();
+    }
+
+    #endregion
+  }
+}
