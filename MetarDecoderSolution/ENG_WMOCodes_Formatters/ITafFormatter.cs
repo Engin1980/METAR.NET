0219fef9f6c239f1f8ecbf64692276553f18c8db
Engin1980_cp <engin@seznam.cz>
2011-06-01 19:33:14 +0000

    Added support for Formatters.

---
+++ MetarDecoderSolution/ENG_WMOCodes_Formatters/ITafFormatter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ENG.WMOCodes.Formatters
+{
+  public interface ITafFormatter
+  {
+    string ToString(ENG.WMOCodes.Codes.Taf taf);
+  }
+}
